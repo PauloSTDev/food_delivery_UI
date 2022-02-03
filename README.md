ef4ba174ae2e2bc9621ace9eca6f5ff0674fed65
Paulo Santos <afonso.mea@gmail.com>
2022-02-03 15:54:00 -0300

    1° commit - Campo de pesquisa, lista de pedidos recentes e restaurants

---
+++ README.md
@@ -0,0 +1,5 @@
+# flutter_food_delivery_ui
+
+## Implementação de um App de Delivery
+
+Tem como objetivo estudar princípios de UI, criando pages, listas de vizualização, campos de pesquisa, e muito mais!
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  _buildRestaurants() {
+    List<Widget> restaurantList = [];
+    restaurants.forEach(
+      (Restaurant restaurant) {
+        restaurantList.add(
+          Container(
+            margin: EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 10.0,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+              border: Border.all(
+                width: 1.0,
+                color: Colors.grey[200],
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    height: 150.0,
+                    width: 150.0,
+                    image: AssetImage(restaurant.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+    return Column(
+      children: restaurantList,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Food Delivery App UI"),
+        leading: IconButton(
+          //Icone de perfil
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          //TextButton, substituição do FlatButton, botão para o carrinho
+          TextButton(
+            child: Text(
+              "Cart (${currentUser.cart.length})",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        //Lista de Widgets
+        children: <Widget>[
+          //Campo de pesquisa(Search Bar)
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              cursorColor: Colors.black54,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      width: 0.8, color: Theme.of(context).primaryColor),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(width: 1.2, color: Colors.deepOrangeAccent),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                hintText: "Search Food or Restaurants",
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 30.0,
+                  color: Colors.black54,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.clear,
+                    color: Colors.black54,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ),
+          //Widget com um list view em colunas dos pedidos recentes
+          RecentOrders(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  "Nearby Restaurants",
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ),
+              _buildRestaurants(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
