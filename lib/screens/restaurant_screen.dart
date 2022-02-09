4fab51d2dfb265636efb6786a567a185342bb77d
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-02-09 20:18:40 -0300

    Adiciona imagem, botão de voltar e favorito

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
 
 class RestaurantScreen extends StatefulWidget {
-
   final Restaurant restaurant;
+
   RestaurantScreen({this.restaurant});
 
   @override
@@ -14,7 +14,43 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Image(
+                height: 220.0,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                image: AssetImage(widget.restaurant.imageUrl),
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      color: Colors.black,
+                      iconSize: 30.0,
+                      onPressed: () => Navigator.pop(context),
+                      icon: Icon(Icons.arrow_back),
+                    ),
+                    IconButton(
+                      color: Theme.of(context).primaryColor,
+                      iconSize: 35.0,
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
