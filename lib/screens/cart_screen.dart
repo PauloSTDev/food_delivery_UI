65ddd76f0717a3acf2d9b1bd525d622f983bf29a
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-02-13 22:27:02 -0300

    Adiciona botão de adicionar e remover, nome do restaurante, nome e preço do produto

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -14,19 +14,104 @@ class _CartScreenState extends State<CartScreen> {
       height: 170.0,
       child: Row(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                width: 150.0,
-                decoration: BoxDecoration(
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 150.0,
+                  decoration: BoxDecoration(
                     image: DecorationImage(
                       image: AssetImage(order.food.imageUrl),
                       fit: BoxFit.cover,
                     ),
-                  borderRadius: BorderRadius.circular(15.0),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          order.food.name,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          order.restaurant.name,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 10.0),
+                        Container(
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            border:
+                                Border.all(width: 0.8, color: Colors.black54),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  "-",
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 20.0),
+                              Text(
+                                order.quantity.toString(),
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(width: 20.0),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  "+",
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: Text(
+                    "\$${order.quantity * order.food.price}",
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
