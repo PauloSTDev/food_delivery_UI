50ed61ecb8c952a28b6eb0e4edda7a1ab28b4fe3
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-03-06 12:10:10 -0300

    Adiciona depend^ncias do firebase e cria pasta firebase_storage_service.dart

---
+++ lib/data/firebase_storage_service.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter/material.dart';
+
+class FirebaseStorageService extends ChangeNotifier {
+  FirebaseStorageService();
+  static Future<dynamic> loadTime(BuildContext context, String Image){
+
+  }
+}
