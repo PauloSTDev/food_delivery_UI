b23fc9b0677194e87509e61de1827b155e577d11
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-03-06 12:22:32 -0300

    Implementa Future do Firebase Storage

--- lib/data/firebase_storage_service.dart
+++ lib/data/firebase_storage_service.dart
@@ -1,10 +1,9 @@
-
-
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class FirebaseStorageService extends ChangeNotifier {
   FirebaseStorageService();
-  static Future<dynamic> loadTime(BuildContext context, String Image){
-
+  static Future<dynamic> loadTime(BuildContext context, String Image) async {
+    return await FirebaseStorage.instance.ref().child(Image).getDownloadURL();
   }
 }
