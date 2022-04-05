b5d96f94dec000962b8a111c895c7da9d0136ec7
Paulo Santos <afonso.mea@gmail.com>
2022-04-05 10:06:07 -0300

    Adiciona physics no ListView

--- lib/widgets/build_restaurants_db.dart
+++ lib/widgets/build_restaurants_db.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
 import 'package:flutter_food_delivery_ui/data/firebase_api.dart';
 import 'package:flutter_food_delivery_ui/models/firebase_file.dart';
+import 'package:flutter_food_delivery_ui/models/restaurant.dart';
 import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
 
 class BuildRestaurantsDB extends StatefulWidget {
@@ -19,80 +21,89 @@ class _BuildRestaurantsDBState extends State<BuildRestaurantsDB> {
 
   @override
   Widget build(BuildContext context) {
-    Widget buildFile(BuildContext context, FirebaseFile file) => Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: 10.0,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15.0),
-                border: Border.all(
-                  width: 1.0,
-                  color: Colors.grey[200],
-                ),
-              ),
-              child: Row(
-                children: [
-                  Hero(
-                    tag: file.url,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(15.0),
-                      child: Image(
-                        height: 150.0,
-                        width: 150.0,
-                        image: NetworkImage(
-                          file.url,
-                        ),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  Container(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            "Nome do restaurante",
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: 4.0),
-                          //RatingStars(restaurant.rating),
-                          Text("Ratings"),
-                          Text(
-                            "Adress",
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: 4.0),
-                          Text(
-                            "0.2 miles away",
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      ),
-                    ),
-                ],
+
+    Widget buildFile(BuildContext context, FirebaseFile file) {
+      List<Widget> restaurantList = [];
+      restaurantList.add(
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 10.0,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+              border: Border.all(
+                width: 1.0,
+                color: Colors.grey[200],
               ),
             ),
-          ],
-        );
+            child: Row(
+              children: [
+                Hero(
+                  tag: file.url,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Image(
+                      height: 150.0,
+                      width: 150.0,
+                      image: NetworkImage(
+                        file.url,
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Nome do restaurante",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(height: 4.0),
+                      //RatingStars(restaurant.rating),
+                      Text("Ratings"),
+                      Text(
+                        "Adress",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        "0.2 miles away",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+      return Column(
+        children: restaurantList,
+      );
+    }
 
-    return Container(
-      child: FutureBuilder<List<FirebaseFile>>(
+
+
+    return FutureBuilder<List<FirebaseFile>>(
         future: futureFiles,
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
@@ -110,28 +121,28 @@ class _BuildRestaurantsDBState extends State<BuildRestaurantsDB> {
                 return Row(
                   children: [
                     Expanded(
-                        child: Container(
-                          height: MediaQuery.of(context).size.height,
-                          width: MediaQuery.of(context).size.width,
-                          margin: EdgeInsets.symmetric(
-                            horizontal: 20.0,
-                            vertical: 10.0,
-                          ),
-                          child: ListView.builder(
-                            itemCount: files.length,
-                            itemBuilder: (context, index) {
-                              final file = files[index];
-                              return buildFile(context, file);
-                        },
+                      child: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 20.0,
+                          vertical: 10.0,
+                        ),
+                        child: ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: files.length,
+                          itemBuilder: (context, index) {
+                            final file = files[index];
+                            return buildFile(context, file);
+                          },
+                        ),
                       ),
-                    ),
                     ),
                   ],
                 );
               }
           }
         },
-      ),
-    );
+      );
   }
 }
