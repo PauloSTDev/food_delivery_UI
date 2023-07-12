0ed35660ded67fde666f6447d845e41414057a77
Paulo Santos <paulo.santos@compasso.com.br>
2023-07-12 17:52:46 -0300

    Atualiza projeto

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/data/data.dart';
 import 'package:flutter_food_delivery_ui/data/firebase_api.dart';
